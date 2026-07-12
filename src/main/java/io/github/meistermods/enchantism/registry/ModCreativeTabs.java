de2a36d370532062031930e100286530ca421211
elecbug <deveb1479@gmail.com>
2026-07-12 12:10:14 +0900

    feat: implement element container filling logic and update related texture

--- src/main/java/io/github/meistermods/enchantism/registry/ModCreativeTabs.java
+++ src/main/java/io/github/meistermods/enchantism/registry/ModCreativeTabs.java
@@ -1,9 +1,12 @@
 package io.github.meistermods.enchantism.registry;
 
 import io.github.meistermods.enchantism.Enchantism;
+import io.github.meistermods.enchantism.element.ElementType;
+import io.github.meistermods.enchantism.item.ElementContainerItem;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -27,6 +30,26 @@ public final class ModCreativeTabs {
                         output.accept(ModBlocks.ELEMENT_INFUSER_ITEM.get());
                         output.accept(ModItems.ELEMENT_CONTAINER.get());
                         output.accept(ModBlocks.COMPRESSED_COBBLESTONE_ITEM.get());
+
+                        ItemStack emptyContainer = new ItemStack(ModItems.ELEMENT_CONTAINER.get());
+
+                        output.accept(emptyContainer);
+
+                        for (ElementType elementType : ElementType.values()) {
+                          if (elementType == ElementType.EMPTY) {
+                            continue;
+                          }
+
+                          ItemStack filledContainer =
+                              new ItemStack(ModItems.ELEMENT_CONTAINER.get());
+
+                          ElementContainerItem.setElement(
+                              filledContainer,
+                              elementType,
+                              ElementContainerItem.MAX_ELEMENT_AMOUNT);
+
+                          output.accept(filledContainer);
+                        }
                       })
                   .build());
 
